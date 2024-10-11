b752280925d587d0ec16c7f1c9f5979b63cbd952
f-dutra <89109328+f-dutra@users.noreply.github.com>
2024-10-10 21:55:44 -0300

    Adiciona a opção de converter taxas

--- calcjuros.c
+++ calcjuros.c
@@ -5,7 +5,8 @@
 #include <locale.h>
 
 float capital, montante, juros, taxa, tempo, parcelas;
-int opt;
+int opt, subopt;
+char aux[6], txt1[6] = " ", txt2[6] = " ";
 
 void clrscr()
 {
@@ -27,9 +28,10 @@ void print_menu(){
 		   "===========================================\n\n"
 
 		   "(1) Calcular juros simples\n"
-		   "(2) Calcular Juros Compostos\n\n"
+		   "(2) Calcular juros compostos\n"
+		   "(3) Converter taxas\n\n"
 
-		   "(3) Sair\n\n"
+		   "(4) Sair\n\n"
 		   );
 
 	if(opt == 404){
@@ -37,7 +39,7 @@ void print_menu(){
 	}
 }
 
-void print_calc(){
+void print_menu_juros(){
 	clrscr();
 	char tipo[10] = "Simples";
 	if (opt == 2){
@@ -48,63 +50,144 @@ void print_calc(){
 		"===========================================\n"
 		" Digite 0 para indicar uma icógnita\n"
 		"-------------------------------------------\n\n", tipo);
-
 }
 
 void perguntas(){
 	printf("Informe o capital inicial: ");
 	scanf("%f", &capital);
+
 	printf("Informe a taxa de juros (sem %%): ");
 	scanf("%f", &taxa);
 	taxa = taxa / 100;
+
 	printf("Informe o tempo/parcelas: ");
 	scanf("%f", &tempo);
+
 	if (capital == 0 || taxa == 0 || tempo == 0){
 		printf("Informe o montante: ");
 		scanf("%f", &montante);
 	}
 }
 
-void print_result(){
-	printf("\nMontante: R$ %.2f\n"
-			"Juros: R$ %.2f\n"
-			"Parcelas: R$ %.2f\n", montante, juros, parcelas);
-}
-
 void func_simples(){
+
 	if (capital == 0){
 		capital = montante / ((taxa*tempo)+1);
-		printf("Capital inicial: R$ %.2f", capital);
+		printf("Capital inicial: R$ %.2f\n", capital);
 	} else if(taxa == 0){
 		taxa = ((montante/capital)-1)/tempo;
-		printf("Taxa de juros: %.2f%%", taxa*100);
+		printf("Taxa de juros: %.2f%%\n", taxa*100);
 	} else if (tempo == 0){
 		tempo = ((montante/capital)+1)/taxa;
-		printf("Tempo de aplicação/parcelas: %.2f", tempo);
+		printf("Tempo de aplicação/parcelas: %.2f\n", tempo);
 	} else {
 		montante = capital*((taxa*tempo)+1);
 		juros = montante - capital;
 		parcelas = montante / tempo;
-		print_result();
+
+		printf("\nMontante: R$ %.2f\n"
+		"Juros: R$ %.2f\n"
+		"Parcelas: R$ %.2f\n", montante, juros, parcelas);
 	}
 }
 
 void func_composto(){
 	if (capital == 0){
 		capital = montante / pow((taxa+1), tempo);
-		printf("Capital inicial: R$ %.2f", capital);
+		printf("Capital inicial: R$ %.2f\n", capital);
 	} else if(taxa == 0){
 		taxa = pow((montante/capital), 1/tempo)-1;
-		printf("Taxa de juros: %.2f%%", taxa*100);
+		printf("Taxa de juros: %.2f%%\n", taxa*100);
 	} else if (tempo == 0){
 		tempo = log((montante/capital))/log((taxa/+1));
-		printf("Tempo de aplicação/parcelas: %.2f", tempo);
+		printf("Tempo de aplicação/parcelas: %.2f\n", tempo);
 	} else {
 		montante = capital*pow((taxa+1), tempo);
 		juros = montante - capital;
 		parcelas = montante / tempo;
-	    	print_result();
+
+		printf("\nMontante: R$ %.2f\n"
+		"Juros: R$ %.2f\n"
+		"Parcelas: R$ %.2f\n", montante, juros, parcelas);
 	}
+}
+
+void print_menu_taxa(){
+	clrscr();
+	printf("===========================================\n"
+		   "\tConverter taxas\n"
+		   "===========================================\n"
+		   " Convertendo de (%s) para (%s)\n"
+		   "-------------------------------------------\n\n"
+
+		   "(1) Diária\t\t(4) Semanal\n"
+		   "(2) Mensal\t\t(5) Trimestral\n"
+		   "(3) Anual\t\t(6) Semestral\n\n"
+
+		   "(7) Voltar\n\n", txt1, txt2
+		   );
+
+	if(opt == 404){
+		printf("Erro! Opção inválida.\n\n");
+	}
+
+}
+
+void texto_taxa(int *o){
+	switch(*o){
+		case 1: strcpy(aux, "a.d."); break;
+		case 2: strcpy(aux, "a.m."); break;
+		case 3: strcpy(aux, "a.a."); break;
+		case 4: strcpy(aux, "a.sm."); break;
+		case 5:	strcpy(aux, "a.t."); break;
+		case 6: strcpy(aux, "a.a."); break;
+		case 7: break;
+		// default: *o = 404; break;  ## Não funciona no momento ##
+	}
+}
+
+int menu_taxa(int o){
+	while(o != 7){
+		print_menu_taxa(o);
+		scanf("%d", &o);
+		texto_taxa(&o);
+		if(o == 404){continue;}
+		else{
+			return o;
+		}
+	}
+}
+
+void convert_mensal(){
+	switch(opt){
+		case 1: taxa *= 30; break;
+		case 2: break;
+		case 3: taxa /= 12; break;
+		case 4: taxa *= 4.28; break;
+		case 5: taxa /= 3; break;
+		case 6: taxa /= 6; break;
+		case 7: break;
+	}
+	printf("Taxa convertida: %.2f%% a.m.", taxa);
+
+}
+
+void calc_taxa(){
+	printf("Informe a taxa: ");
+	scanf("%f", &taxa);
+	printf("Taxa convertida: %.2f%% %s", taxa, txt1);
+
+	convert_mensal();
+	switch(subopt){
+		case 1: taxa /= 30; break;
+		case 2: break;
+		case 3: taxa *= 12; break;
+		case 4: taxa /= 4.28; break;
+		case 5: taxa *= 3; break;
+		case 6: taxa *= 6; break;
+		case 7: break;
+	}
+	printf("Taxa convertida: %.2f%% %s", taxa, txt2);
 }
 
 void continuar(){
@@ -114,33 +197,57 @@ void continuar(){
 			printf("\n\nRealizar outro cálculo (s/n)? ");
 			scanf("%c", &c);
 			if (c == 'n'){
-				opt = 0;
+				opt = 7;
 			}
 }
 
 
 
 int main(){
-	setlocale(LC_ALL, "Portuguese");
-	while(opt != 3){
+	setlocale(LC_ALL, "portuguese");
+	while(opt != 4){
 		print_menu();
 		scanf(" %d", &opt);
 
-		if(opt == 1 || opt == 2 ){
-		while (opt != 0){
-			clrscr();
-			print_calc();
+		switch(opt){
+		case 1: case 2:
+		while (opt != 7){
+			print_menu_juros();
 			perguntas();
 
 			if (opt == 1){func_simples();}
 			else{func_composto();}
+
 			continuar();
 			}
-		}
-		else if (opt == 3){
+			break;
+		case 3:
+		while(opt != 7){
+			opt = menu_taxa(opt);
+			if (opt == 7){break;}
+			strcpy(txt1, aux);
+
+			subopt = menu_taxa(subopt);
+			if (subopt == 7){
+				subopt = 0;
+				strcpy(txt1, " ");
+				continue;
+			}
+			strcpy(txt2, aux);
+			print_menu_taxa();
+
+			calc_taxa();
+
+			continuar();
+			strcpy(txt1, " ");
+			strcpy(txt2, " ");
+		} break;
+		case 4:
 			printf("Encerrando o programa...");
-		} else{
+			break;
+		default:
 			opt = 404;
+			break;
 		}
 	}
 }
